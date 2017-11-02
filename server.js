43d3052e19fbb8340f24ed2ef0ec48a7eae8c11f
Viktor Shcheglov <vscheglov@odin.com>
2017-11-02 15:25:52 +0300

    update buid

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 5000;
 // Priority serve any static files.
 app.use(express.static(path.resolve(__dirname)));
 
-// Answer API requests.
 app.get('/dashboard', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'dashboard.html'));
 });
