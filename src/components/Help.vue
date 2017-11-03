928a28bbdfea3ef4d65a3430c2dd23e52631725a
Viktor Shcheglov <vscheglov@odin.com>
2017-11-03 10:40:20 +0300

    update help

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="help-hint">
     <div class="help-desc">Press "esc" to show your cursor</div>
-    <modal name="help" class="help-container" width="800" height="500">
+    <modal name="help" class="help-container" width="800" height="450">
       <div v-html="help"></div>
       <button class="help-close" @click="$modal.hide('help')">
         ❌
