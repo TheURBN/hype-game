ff291cf10faf152c289bdd7c21ad31f4d7048efb
Viktor Shcheglov <vscheglov@odin.com>
2017-11-07 01:01:09 +0300

    add dialog about controls

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="help-hint">
-    <div class="help-desc">press "esc" to reveal mouse pointer</div>
+    <div class="help-desc">press <strong>"esc"</strong> to reveal mouse pointer</div>
+    <div class="help-desc">press <strong>"h"</strong> for game contols hint</div>
+
     <modal name="help" class="help-container" width="800" height="450">
       <div v-html="help"></div>
       <button class="help-close" @click="$modal.hide('help')">
+        ❌
+      </button>
+    </modal>
+    <modal name="helpContols" class="help-container" width="500" height="450">
+      <img class="help-controls" src="/dist/img/game-controls.svg"/>
+      <button class="help-close" @click="$modal.hide('helpContols')">
         ❌
       </button>
     </modal>
@@ -22,6 +30,11 @@ export default {
     return {
       help: template,
     }
+  },
+  created() {
+    window.addEventListener('keydown', (e) => {
+      if (e.keyCode === 72) this.$modal.show('helpContols');
+    });
   },
 };
 </script>
